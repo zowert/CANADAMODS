0ab8dbf095c1059ef0f92abfa34f0f6427c2cef4
zowert <143289782+zowert@users.noreply.github.com>
2023-11-19 20:49:39 +0000

    ff

--- src/pages/GunPack.vue
+++ src/pages/GunPack.vue
@@ -12,6 +12,10 @@
       </DownloadableItem>
       <DownloadableItem :image="gunImage6" link="https://drive.google.com/drive/folders/1n58gAz4epRIVit7jGv2fuXG30ocXg-mw">
       </DownloadableItem>
+    <DownloadableItem :image="gunImage7" link="https://drive.google.com/file/d/1dg6DUyu_3fHz7g8KeGCNpGs-RiVg4HnE/view?usp=sharing">
+      </DownloadableItem>
+    <DownloadableItem :image="gunImage8" link="https://drive.google.com/file/d/1-NgRvlr5_pXEP8FzVAXOJF5Mjwtwjwap/view">
+      </DownloadableItem>
   </GridDisplay>
 </template>
 
@@ -23,6 +27,9 @@ import gunImage3 from './../assets/images/4rtyynovyypng_1690547250.png'
 import gunImage4 from './../assets/images/323.png'
 import gunImage5 from './../assets/images/232.png'
 import gunImage6 from './../assets/images/LEGOPBMODSgg-removebg-preview.png'
+import gunImage7 from './../assets/images/LPBModskkk-removebg-preview.png'
+import gunImage8 from './../assets/images/PBModshh-removebg-preview.png'
+import gunImage9 from './../assets/images/3b3267e6d1a08686hhhhh-removebg-preview.png'
 import DownloadableItem from "../components/DownloadableItem.vue" 
 export default {
   components: {DownloadableItem, GridDisplay},
@@ -34,6 +41,9 @@ export default {
       gunImage4,
       gunImage5,
       gunImage6,
+      gunImage7,
+      gunImage8,
+      gunImage9,
     }
   }  
 }
